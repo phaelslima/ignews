00d8fe53022e3cc965035d01217d1ac0b9c986e1
Raphael Lima <phaelslima@gmail.com>
2022-03-24 06:44:49 -0300

    feat: configure next-auth (github)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { AppProps } from "next/app"
+import { SessionProvider as NextAuthProvider } from "next-auth/react"
+
 import { Header } from "../components/Header"
 
 import '../styles/global.scss'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <>
-    <Header />
-    <Component {...pageProps} />
-    </>
+    <NextAuthProvider session={session}>
+      <Header />
+      <Component {...pageProps} />
+    </NextAuthProvider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 
 import { stripe } from "../services/stripe";
@@ -41,7 +41,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KgdYiImFaKSfkkFrOeOFp6K')
 
   const product = {
@@ -55,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
