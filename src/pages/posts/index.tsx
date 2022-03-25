28dedefa99db9c9d0f79b0d1a000b4e688b0d5e9
Raphael Lima <phaelslima@gmail.com>
2022-03-25 20:20:58 -0300

    feat: add post and preview pages

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 
 import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
@@ -7,6 +8,7 @@ import { RichText } from 'prismic-dom'
 import { getPrimiscClient } from '../../services/prismic'
 
 import styles from './styles.module.scss'
+
 
 type Post = {
   slug: string
@@ -29,11 +31,13 @@ export default function Posts({ posts }: PostProps) {
     <main className={styles.container}>
       <div className={styles.posts}>
         {posts.map(post => (
-          <a key={post.slug}>
-            <time>{post.updatedAt}</time>
-            <strong>{post.title}</strong>
-            <p>{post.excerpt}</p>
-          </a>
+          <Link href={`/posts/${post.slug}`}>
+            <a key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          </Link>
         ))}
       </div>
     </main>
